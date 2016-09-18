e087ff79e6689ba49ac07da66a12d7133cdffaea
GaddPetrivoki <gaddpetrivoki@gmail.com>
2016-09-17 19:32:22 -0500

    Added javascript file to detect mobile users and change the page accordingly.

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+		$("#git-col").append("<br>");
+ 
+ 
+	}
+}
